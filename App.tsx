b21fae6bc9e124d59320dd4e76aea2edb3b396b1
Abhijeet <108912315+lets-do-code@users.noreply.github.com>
2023-11-10 14:12:29 +0530

    connect cognito user to database user

--- App.tsx
+++ App.tsx
@@ -6,14 +6,82 @@
  */
 
 import "react-native-gesture-handler"
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView, StatusBar } from 'react-native';
 import { withAuthenticator } from "aws-amplify-react-native"
 import RootNavigation from "./src/navigation";
+import { Auth, API, graphqlOperation } from "aws-amplify";
+import { createUser } from "./src/graphql/mutations"
+import { getUser } from "./src/graphql/queries";
 
 
+const randomImages = [
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-2.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-3.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-6.jpg',
+  'https://hieumobile.com/wp-content/uploads/avatar-among-us-9.jpg',
+];
+
+
+const getRandomImage = () => {
+  return randomImages[Math.floor(Math.random() * randomImages.length)]
+};
 
 function App(): JSX.Element {
+
+
+  useEffect(() => {
+
+    const fetchUser = async () => {
+      //get currently authenticated user
+
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+      console.log(userInfo)
+
+      if (!userInfo) {
+        return;
+      }
+
+
+      // check if the user exists in database
+      const getUserResponse = await API.graphql(
+        graphqlOperation(
+          getUser,
+          {
+            id: userInfo.attributes.sub
+          }
+        )
+      )
+
+
+      if (getUserResponse.data.getUser) {
+        console.log("user already exists")
+        return;
+      }
+
+      // if it's a new user,create a new one
+
+      const newUser = {
+        id: userInfo.attributes.sub,
+        username: userInfo.username,
+        email: userInfo.attributes.email,
+        imageUri: getRandomImage(),
+      }
+
+      await API.graphql(
+        graphqlOperation(
+          createUser,
+          {
+            input: newUser
+          }
+        )
+      )
+      console.log("New User", newUser)
+
+
+    }
+    fetchUser();
+  }, [])
 
   return (
     <>
